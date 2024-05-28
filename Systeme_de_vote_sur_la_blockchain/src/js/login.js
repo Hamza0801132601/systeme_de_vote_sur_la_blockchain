5d20761ef52077aa19252f82ebb6cfa6858e8fcd
Hamza0801132601 <hamalfa01@gmail.com>
2024-05-29 00:25:24 +0100

    la premiere version de notre systeme

---
+++ Systeme_de_vote_sur_la_blockchain/src/js/login.js
@@ -0,0 +1,36 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const voter_id = document.getElementById('voter-id').value;
+  const password = document.getElementById('password').value;
+  const token = voter_id;
+
+  const headers = {
+    'method': "GET",
+    'Authorization': `Bearer ${token}`,
+  };
+
+  fetch(`http://127.0.0.1:8000/login?voter_id=${voter_id}&password=${password}`, { headers })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Login failed');
+    }
+  })
+  .then(data => {
+    if (data.role === 'admin') {
+      console.log(data.role)
+      localStorage.setItem('jwtTokenAdmin', data.token);
+      window.location.replace(`http://127.0.0.1:8080/admin.html?Authorization=Bearer ${localStorage.getItem('jwtTokenAdmin')}`);
+    } else if (data.role === 'voter'){
+      localStorage.setItem('jwtTokenVoter', data.token);
+      window.location.replace(`http://127.0.0.1:8080/index.html?Authorization=Bearer ${localStorage.getItem('jwtTokenVoter')}`);
+    }
+  })
+  .catch(error => {
+    console.error('Login failed:', error.message);
+  });
+});
